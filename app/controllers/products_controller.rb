5b3ac126c3df90e61e77f86255767c9e7a7c38bb
Luke <luke.e.philips@gmail.com>
2017-02-10 14:03:12 -0800

    add conditionals to view to only give crud C,U,D functionality to signed in users

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, :except => [:show, :index]
+  
   def index
     @products = Product.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'products#index'
+  authenticate :user do
+    resources :fjords, only: [:new, :create, :edit, :update, :destroy]
+  end
   resources :products
 end
